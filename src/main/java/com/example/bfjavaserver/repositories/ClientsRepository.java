8ad13b7246b65da3f0776b09e3c9dc7c130024c6
itpark <a.m.smyslov@yandex.ru>
2022-01-07 21:35:17 +0300

    add simple auth

--- src/main/java/com/example/bfjavaserver/repositories/ClientsRepository.java
+++ src/main/java/com/example/bfjavaserver/repositories/ClientsRepository.java
@@ -2,7 +2,10 @@ package com.example.bfjavaserver.repositories;
 
 import com.example.bfjavaserver.models.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ClientsRepository extends JpaRepository<Client, Integer> {
+    //@Query(value = "select * from clients where email=?1 and password=?2", nativeQuery = true)
+    Client findByEmailAndPassword(String email, String password);
 }
 
