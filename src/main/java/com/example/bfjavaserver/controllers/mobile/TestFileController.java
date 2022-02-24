44c5da781050f027c7b911015ebfb293ce17ec1f
itpark <a.m.smyslov@yandex.ru>
2022-02-24 21:43:44 +0300

    test get file

--- src/main/java/com/example/bfjavaserver/controllers/mobile/TestFileController.java
+++ src/main/java/com/example/bfjavaserver/controllers/mobile/TestFileController.java
@@ -1,12 +1,13 @@
 package com.example.bfjavaserver.controllers.mobile;
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+
+import org.apache.commons.io.IOUtils;
 
 import java.io.FileWriter;
+import java.io.InputStream;
 
 @RestController
 @RequestMapping(path = "/mobile/test", method = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
@@ -14,10 +15,16 @@ import java.io.FileWriter;
 public class TestFileController {
 
     @GetMapping(value = "/createFile")
-    public void createFile() throws Exception{
+    public void createFile() throws Exception {
         FileWriter fileWriter = new FileWriter("test.txt");
         fileWriter.write("hello world");
         fileWriter.close();
+    }
 
+    @GetMapping(value = "/getFile")
+    public @ResponseBody
+    byte[] getImage() throws Exception {
+        InputStream inputStream = getClass().getResourceAsStream("test.txt");
+        return IOUtils.toByteArray(inputStream);
     }
 }
