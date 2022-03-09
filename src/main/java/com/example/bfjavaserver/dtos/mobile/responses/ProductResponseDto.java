233113c00dcbaa7f3312458907ce835ece0137b6
Innokentii <jacksafj@gmail.com>
2022-03-09 17:54:33 +0300

    нуждается ли единственный коммит комментария, если название ветки само за себя говорит?

---
+++ src/main/java/com/example/bfjavaserver/dtos/mobile/responses/ProductResponseDto.java
@@ -0,0 +1,19 @@
+package com.example.bfjavaserver.dtos.mobile.responses;
+
+import com.example.bfjavaserver.models.ProductCategory;
+import lombok.Data;
+
+@Data
+public class ProductResponseDto {
+	public int id;
+	public String name;
+	public int price;
+	public String priceMeasurementName;
+	public int amount;
+	public String amountMeasurementName;
+	public String description;
+	public double rating;
+	public String photoPath;
+	public FarmerResponseDto farmer;
+	public ProductCategory productCategory;
+}
