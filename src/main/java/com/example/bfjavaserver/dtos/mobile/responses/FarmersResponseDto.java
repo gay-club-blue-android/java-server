ecbac5b61d20e17993c09b3b565baf65c38a374b
Innokentii <jacksafj@gmail.com>
2022-03-16 15:31:26 +0300

    done

---
+++ src/main/java/com/example/bfjavaserver/dtos/mobile/responses/FarmersResponseDto.java
@@ -0,0 +1,10 @@
+package com.example.bfjavaserver.dtos.mobile.responses;
+
+import lombok.AllArgsConstructor;
+
+import java.util.ArrayList;
+@AllArgsConstructor
+public class FarmersResponseDto {
+    public ArrayList<FarmerResponseDto> products;
+}
+
