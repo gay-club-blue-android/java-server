f648cc6b672ae6b3084f7259537dd0cdf189bea5
Innokentii <jacksafj@gmail.com>
2022-05-17 19:33:45 +0300

    comment all configs

--- src/main/java/com/example/bfjavaserver/config/ApplicationConfig.java
+++ src/main/java/com/example/bfjavaserver/config/ApplicationConfig.java
@@ -8,13 +8,23 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * Класс отвечающий за подключение дополнительных отбработчиков запросов к серверу
+ */
 @Configuration
 @AllArgsConstructor
 public class ApplicationConfig implements WebMvcConfigurer {
-
+    /**
+     * Сервис для продвинутого доступа к таблице API-ключей
+     */
     @Autowired
     private final AppsApiKeysService appsApiKeysService;
 
+    /**
+     * добавляет обработчик события, вызываемого перед каждым запросом
+     * @param registry - сущность, занимающаяся добавлением обработчиков
+     * @see AuthInterceptor
+     */
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new AuthInterceptor(appsApiKeysService));
--- src/main/java/com/example/bfjavaserver/config/ModelMapperConfig.java
+++ src/main/java/com/example/bfjavaserver/config/ModelMapperConfig.java
@@ -4,6 +4,10 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * Класс, регистирующий modelMapper для приложения
+ * @see ModelMapper
+ */
 @Configuration
 public class ModelMapperConfig {
     @Bean
