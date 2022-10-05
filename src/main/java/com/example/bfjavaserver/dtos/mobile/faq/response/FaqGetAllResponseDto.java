d701401aa6c1cbb8cc70ce71c3ddc3db1f2d9703
itpark <a.m.smyslov@yandex.ru>
2022-10-05 22:34:46 +0300

    finish current refactor

--- src/main/java/com/example/bfjavaserver/dtos/mobile/faq/response/FaqGetAllResponseDto.java
+++ src/main/java/com/example/bfjavaserver/dtos/mobile/faq/response/FaqGetAllResponseDto.java
@@ -1,10 +1,8 @@
 package com.example.bfjavaserver.dtos.mobile.faq.response;
 
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import lombok.Data;
 
+@Data
 public class FaqGetAllResponseDto {
     public int id;
     public String question;
